8e849067f48f8f6580a977c693a766108c705d0f
aarondominic1 <aarondominic002@gmail.com>
2024-01-01 15:28:24 +0000

    Creating the first visulisation

--- code/Project.py
+++ code/Project.py
@@ -1,11 +1,12 @@
 # Importing Libraries 
 
 import pandas as pd 
-import matplotlib as plt 
+import matplotlib.pyplot as plt 
 
 #Reading the data file into project 
 student_data = pd.read_csv("students.csv")
 print(student_data)
+
 
 #Hypothesis: The academic success of students is influenced by a combination of demographic, socio-economic, and educational factors.
 
@@ -17,4 +18,3 @@ plt.title('Relationship between Previous Qualifications and Academic Success')
 plt.xlabel('Previous Qualification Grade')
 plt.ylabel('Average Academic Success')
 plt.show()
-
